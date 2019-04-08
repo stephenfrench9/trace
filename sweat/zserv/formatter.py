95b529b03ab2d43b214e88276a79d382a7d34e38
Stephen French <stephenfrench9@gmail.com>
2019-04-08 11:14:32 -0700

    server z breaks with 20% probablility

--- sweat/zserv/formatter.py
+++ sweat/zserv/formatter.py
@@ -1,14 +1,29 @@
 from flask import Flask
 from flask import request
 from lib.tracing import init_tracer
+from time import sleep
 from opentracing.ext import tags
 from opentracing.propagation import Format
+from random import randint
 
 app = Flask(__name__)
 tracer = init_tracer('zserv')
- 
+slow = False
+
+
 @app.route("/format")
 def format():
+    global slow
+    if randint(1, 5) == 5:
+        slow = True
+
+    if(slow):
+        print("The server is slow")
+        sleep(2)
+        print("done sleeping")
+    else:
+        print("the server is fast")
+
     span_ctx = tracer.extract(Format.HTTP_HEADERS, request.headers)
     span_tags = {tags.SPAN_KIND: tags.SPAN_KIND_RPC_SERVER}
     with tracer.start_active_span('end-chain', child_of=span_ctx, tags=span_tags) as scope:
